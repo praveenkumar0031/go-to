401195912db63beb045848a17c592e4dd5714ec4
Praveen kumar S <praveen0031kumar@gmail.com>
2026-06-01 15:12:24 +0530

    project structure defined

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose =require("mongoose");
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URL;
+    if (!uri) throw new Error("MONGO_URI is not defined in .env");
+    await mongoose.connect(uri);
+    console.log("MongoDB Connection established");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports=connectDB;
